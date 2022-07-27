cbce783a55a93f97c01b6af8ad4aa99e7c0d2842
Anokhautomation <67797819+anokhramesh@users.noreply.github.com>
2022-07-27 22:25:19 +0400

    Update GUISerialcommunication.ino

--- GUISerialcommunication.ino
+++ GUISerialcommunication.ino
@@ -1,14 +1,14 @@
 //GUI serial communication
-String data;// variable data is string type
-char  d1;// variable di is char type
+String data;// initilize variable data is string type
+char  d1;//initilize variable d1 is char type
 void setup()
 {
-  Serial.begin(9600);// baud rate for serial communication
-  pinMode(13, OUTPUT);// set digital pin 13 as an output
+  Serial.begin(9600);// set baud rate for serial communication
+  pinMode(13, OUTPUT);// define digital pin 13 as an output
 }
 void loop()
 {
-  if (Serial.available())// checking any data is available is available on serial
+  if (Serial.available())// checking any data is available on serial port.
   {
     data = Serial.readString();// serialdata will store to data variable
     d1 = data.charAt(0);//d1 variable will store the character available
